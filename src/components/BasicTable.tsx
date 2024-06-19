42f65e72272df83742636a7ca8fd53f084fcc37a
GiovanniSambonino <gsamboni@espol.edu.ec>
2024-06-19 14:42:40 -0500

    update image in table

--- src/components/BasicTable.tsx
+++ src/components/BasicTable.tsx
@@ -15,16 +15,17 @@ function createData(
   fat: number,
   carbs: number,
   protein: number,
+  image: string,
 ) {
-  return { name, calories, fat, carbs, protein };
+  return { name, calories, fat, carbs, protein, image };
 }
 
 const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
+  createData('Frozen yoghurt', 159, 6.0, 24, 4.0,'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-jpVGhsCffJLP5zwHKbBswWEPAuX0y-uEpQ&s'),
+  createData('Ice cream sandwich', 237, 9.0, 37, 4.3,'https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/IceCreamSandwich.jpg/1200px-IceCreamSandwich.jpg'),
+  createData('Eclair', 262, 16.0, 24, 6.0,'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSq_Nt-jSSTsnpjcsR7RxEhJzX6f3YhdwouDA&s'),
+  createData('Cupcake', 305, 3.7, 67, 4.3,'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTiKEyEdBYYiOZQlcJwDs7bOVNn3WtjCMzfxg&s'),
+  createData('Gingerbread', 356, 16.0, 49, 3.9,'https://www.skinnytaste.com/wp-content/uploads/2011/12/low-fat-gingerbread-men-500x425.jpg'),
 ];
 
 export default function BasicTable() {
@@ -38,6 +39,7 @@ export default function BasicTable() {
             <TableCell align="right">Fat&nbsp;(g)</TableCell>
             <TableCell align="right">Carbs&nbsp;(g)</TableCell>
             <TableCell align="right">Protein&nbsp;(g)</TableCell>
+            <TableCell align="center">Example</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
@@ -50,6 +52,9 @@ export default function BasicTable() {
               <TableCell align="right">{row.fat}</TableCell>
               <TableCell align="right">{row.carbs}</TableCell>
               <TableCell align="right">{row.protein}</TableCell>
+              <TableCell align="center">
+              <img src={row.image} alt={row.name} style={{ maxWidth: '50%', height: 'auto' }} />
+              </TableCell>
             </TableRow>
           ))}
         </TableBody>
