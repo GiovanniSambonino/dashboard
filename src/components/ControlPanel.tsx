98f493388a9228846959000c766bbd09634aee00
GiovanniSambonino <gsamboni@espol.edu.ec>
2024-06-24 14:33:12 -0500

    update

--- src/components/ControlPanel.tsx
+++ src/components/ControlPanel.tsx
@@ -80,11 +80,11 @@ export default function ControlPanel() {
 
 
             {/* Muestra la descripción de la variable seleccionada */}
-            <Typography mt={2} component="p" color="text.secondary">
+            {/* <Typography mt={2} component="p" color="text.secondary">
                 {
                     (selected >= 0) ? items[selected]["description"] : ""
                 }
-            </Typography>
+            </Typography> */}
 
             <Typography ref={descriptionRef} mt={2} component="p" color="text.secondary" />
 		
